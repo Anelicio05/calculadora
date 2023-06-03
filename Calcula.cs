52586fe6cc579eb9513e411e3cdca1c2efb72b35
Anelicio Santos <an.eli.cio05@gmail.com>
2023-06-03 15:54:09 -0300

    first commit

---
+++ Calcula.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Calcula
+    {
+        public int soma(int num1, int num2){
+            return num1 + num2;
+        }
+
+        public int Subtracao(int num1, int num2){
+            return num1 - num2;
+        }
+
+         public int Divisao(int num1, int num2){
+            return num1 / num2;
+        }
+
+        public int Multiplicacao(int num1, int num2){
+            return num1 * num2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Calculadora;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calcula calculadora = new Calcula();
+
+            Console.WriteLine("Qual operação deseja fazer:");
+            Console.WriteLine("1-Soma:");
+            Console.WriteLine("2-Subtração:");
+            Console.WriteLine("3-Divisão:");
+            Console.WriteLine("4-Multiplicação:");
+            Console.Write("Escolha a operação:");
+            int op = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+            
+            if(op == 1){
+                int res = calculadora.soma(num1, num2);
+
+                Console.WriteLine("Resultado: " + res);
+            }
+             if(op == 2){
+                int res = calculadora.Subtracao(num1, num2);
+
+                Console.WriteLine("Resultado: " + res);
+            }
+
+             if(op == 3){
+                int res = calculadora.Divisao(num1, num2);
+
+                Console.WriteLine("Resultado: " + res);
+            }
+
+             if(op == 4){
+                int res = calculadora.Multiplicacao(num1, num2);
+
+                Console.WriteLine("Resultado: " + res);
+            }
+
+
+
+        }
+    }
+}
